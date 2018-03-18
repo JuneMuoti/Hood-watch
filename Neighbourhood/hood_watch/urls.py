0df3fe79d695c078ac81338a03cb54ba55aa4a3f
June Muoti <june.muoti@gmail.com>
2018-03-18 22:10:04 +0300

    add url for posts,hood,new profile

--- Neighbourhood/hood_watch/urls.py
+++ Neighbourhood/hood_watch/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
-     url('^$',views.index,name = 'index'),
+
+     url('^$',views.index,name = 'landing'),
+      url(r'^posts/',views.post,name = 'post'),
+        url(r'^hood/',views.my_hood,name = 'my_hood'),
+        url(r'^join/hood/',views.my_hood,name = 'join_hood'),
+        url(r'^leave/hood/',views.my_hood,name = 'leave_hood'),
+       url(r'^new/profile$', views.Profile, name='new_profile'),
+       url(r'^new/post$', views.new_post, name='new_post'),
+
 
  ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
