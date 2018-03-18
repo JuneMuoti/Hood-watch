a8c3b1f63a4584beae83599f70d123d74e52ff37
June Muoti <june.muoti@gmail.com>
2018-03-18 22:06:55 +0300

    add functions for Neighbourhood class

--- Neighbourhood/hood_watch/models.py
+++ Neighbourhood/hood_watch/models.py
@@ -10,8 +10,19 @@ class Neighbourhood(models.Model):
     occoupants_count=models.IntegerField()
     user_admin=models.ForeignKey(User)
 
+
     def __str__(self):
         return self.name
+    def create_neigborhood():
+        self.save()
+    def delete_neigborhood():
+        objects.filter(id = 2).delete()
+    def get_hood():
+        hood= Neighbourhood.objects.get(id =1)
+        return hood
+    # def find_neigborhood(neigborhood_id):
+    # def update_neighborhood():
+    # def update_occupants():
 
 class Business(models.Model):
     name=models.CharField(max_length=60)
@@ -20,11 +31,16 @@ class Business(models.Model):
     business_email=models.CharField(max_length=60)
     def __str__(self):
         return self.name
+    # def create_business():
+    #     self.save()
+    # def delete_business():
+    # def find_business(business_id):
+    # def update_business():
 class User(models.Model):
     name=models.CharField(max_length=60)
     profile_pic=models.ImageField(upload_to='images/',blank=True)
     user_id=models.IntegerField()
-    hood=models.ForeignKey(Neighbourhood)
+    hood=models.ForeignKey(Neighbourhood,null=True)
     user_email=models.CharField(max_length=60)
     def __str__(self):
         return self.name
@@ -34,12 +50,14 @@ class Post(models.Model):
     posted_by=models.ForeignKey(User,on_delete=models.CASCADE)
     pub_date=models.DateTimeField(auto_now_add=True)
     post_image=models.ImageField(upload_to='images/',blank=True,null=True)
+    hood=models.ForeignKey(Neighbourhood,null=True)
     def __str__(self):
         return self.title
     @classmethod
     def get_post(cls):
         post=Post.objects.all()
         return post
+
 
 
 
