4ceb2e1f8f9fe2a958ae242b795c9ee62b09f3e2
June Muoti <june.muoti@gmail.com>
2018-03-19 18:50:12 +0300

    regisrer app models

---
+++ hood_watch/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import User,Business,Neighbourhood,Post
+
+
+
+admin.site.register(User)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+
+# Register your models here.
